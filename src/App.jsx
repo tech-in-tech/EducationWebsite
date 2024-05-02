f16d2d67846d9d4aa2d9033345ac8a064e71749a
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-05-03 00:21:12 +0530

    create about component and add it to app.js \n and add transition to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <div className="container">
         <Title subTitle = "Our PROGRAM" title = "What We Offer"/>
       <Programs/>
+      <About/>
       </div>
        
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png';
 
 
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 100 ? setSticky(true) : setSticky(false)
+    })
+  }, [])
   return (
-    <nav className='container'>
-      <img src={logo} alt="" className='logo'/>
+    <nav className={`container ${sticky?'dark-nav':''}`}>
+      <img src={logo} alt="" className='logo' />
       <ul>
         <li>Home</li>
         <li>Programs</li>
