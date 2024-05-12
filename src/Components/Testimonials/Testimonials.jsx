406a4e6dab84bc65a4618b8b5ccb2fd0d4d1eb34
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-05-12 12:56:18 +0530

    add slider funclionality to testimonial component

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testimonials.css'
 import next_icon from "../../assets/next-icon.png"
 import back_icon from "../../assets/back-icon.png"
@@ -10,25 +10,33 @@ import user_4 from "../../assets/user-4.png"
 
 
 const Testimonials = () => {
+
+  const slider = useRef();
+  let tx = 0;
+
+const slideForward = ()=>{
+  if(tx > -50){
+    tx -= 25;
+  }
+
+  slider.current.style.transform = `translateX(${tx}%)`
+}
+
+
+const slideBckword = ()=>{
+  if(tx < 0){
+    tx += 25;
+  }
+
+  slider.current.style.transform = `translateX(${tx}%)`
+}
+
   return (
     <div className='testimonials'>
-      <img className='next-btn' src={next_icon} alt="" />      
-      <img className='back-btn' src={back_icon} alt="" />      
+      <img className='next-btn' src={next_icon} alt="" onClick={slideForward}/>      
+      <img className='back-btn' src={back_icon} alt="" onClick={slideBckword}/>      
       <div className='slider'>
-        <ul>
-          <li>
-            <div className="slide">
-              <div className="user-info">
-                <img src={user_4} alt="" />
-                <div>
-                  <h3>William Jackson</h3>
-                  <span>Edisity, USA</span>
-                </div>
-              </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta officia consequatur deleniti libero nesciunt laborum laboriosam laudantium provident dicta ab?</p>
-            </div>
-          </li>
-
+        <ul ref={slider}>
 
           <li>
             <div className="slide">
@@ -62,6 +70,20 @@ const Testimonials = () => {
             <div className="slide">
               <div className="user-info">
                 <img src={user_3} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edisity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta officia consequatur deleniti libero nesciunt laborum laboriosam laudantium provident dicta ab?</p>
+            </div>
+          </li>
+
+
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
                 <div>
                   <h3>William Jackson</h3>
                   <span>Edisity, USA</span>
