ea8fb52a3ca85bc74aface0c3ac14a7f0a940a7d
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-05-15 21:53:40 +0530

    complete the contact component and email sending functionality to form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,6 +7,33 @@ import location_icon from '../../assets/location-icon.png'
 import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "810536a2-129b-4eb0-98f9-57c11952d368");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
   return (
     <div className='contact'>
       <div className="contact-col">
@@ -19,7 +46,7 @@ const Contact = () => {
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
           <label>Your name</label>
           <input type="text" name='name' placeholder='Enter your name' required />
           <label>Phone Number</label>
@@ -28,6 +55,7 @@ const Contact = () => {
           <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
           <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
         </form>
+        <span>{result}</span>
       </div>
       
     </div>
